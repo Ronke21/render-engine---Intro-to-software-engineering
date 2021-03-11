3574f87969f63283a2e7b25c3213b23e80361ea4
רון קינן <ronke21@gmail.com>
2021-03-12 00:35:20 +0200

    added cylinder getNormal and test for it

--- src/geometries/Cylinder.java
+++ src/geometries/Cylinder.java
@@ -14,9 +14,10 @@ public class Cylinder extends Tube {
 
     /**
      * constructor for cylinder.
+     *
      * @param axisRay - ray for base tube
-     * @param radius = radius of base tube
-     * @param height - length of cylinder
+     * @param radius  = radius of base tube
+     * @param height  - length of cylinder
      */
     public Cylinder(Ray axisRay, double radius, double height) {
         super(axisRay, radius);
@@ -38,9 +39,36 @@ public class Cylinder extends Tube {
     }
 
     @Override
-    public Vector getNormal(Point3D point) {
-        // if point is on bases:
-        return getAxisRay().getDir();
+    public Vector getNormal(Point3D P) {
+
+        Point3D P0 = _axisRay.getP0(); //middle of starting base
+        Vector v = _axisRay.getDir();
+        Point3D P1 = P0.add(v.scale(_height)); //middle of far base
+
+        //if point is on the starting base - if distance from p0 is radius, and orthogonal to ray (so it is not on ray itself)
+        if ((P.distance(P0) <= _radius) && (P.subtract(P0).dotProduct(v) == 0)) {
+            return getAxisRay().getDir().scale(-1);
+        }
+        //if point is on the far base - if distance from p1 is radius, and orthogonal to ray (so it is not on ray itself)
+        else if ((P.distance(P1) <= _radius) && (P.subtract(P1).dotProduct(v) == 0)) {
+            return getAxisRay().getDir();
+        }
+
+        // if point is on round surfaces - not based:
+        else {
+
+            Vector PMinusP0 = P.subtract(P0);
+            double t = v.dotProduct(PMinusP0);
+
+            Point3D O = P0.add(v.scale(t));
+
+            Vector sub = P.subtract(O);
+
+            sub.normalize();
+
+            return sub;
+            //copy from tube
+        }
 
 
     }
--- tests/geometries/CylinderTest.java
+++ tests/geometries/CylinderTest.java
@@ -17,11 +17,28 @@ class CylinderTest {
     void getNormal() {
 
         // ============ Equivalence Partitions Tests ==============
-        Ray ray = new Ray(new Point3D(0,1,0), new Vector(0,1,0));
-        Cylinder cl = new Cylinder(ray, 2, 100);
+        Ray ray = new Ray(new Point3D(0, -2, 0), new Vector(0, 1, 0));
+        Cylinder cl = new Cylinder(ray, 2, 3);
 
         // Test that result of getNormal is proper
-        assertEquals(cl.getNormal(null), new Vector(0,1,0));
+
+        //TC01 - inside first base:
+        assertEquals(cl.getNormal(new Point3D(0, -2, 1)), new Vector(0, -1, 0));
+
+        //TC02 - inside far base:
+        assertEquals(cl.getNormal(new Point3D(0, 1, 1)), new Vector(0, 1, 0));
+
+        //TC03 - round surface:
+        assertEquals(cl.getNormal(new Point3D(0, 0, 2)), new Vector(0, 0, 1));
+
+
+        // =============== Boundary Values Tests ==================
+        //TC10 - corner first base, normal should be like inside base
+        assertEquals(cl.getNormal(new Point3D(0, -2, 2)), new Vector(0, -1, 0));
+
+        //TC11 - corner far base - normal should be like inside base
+        assertEquals(cl.getNormal(new Point3D(0, 1, 2)), new Vector(0, 1, 0));
+
 
     }
 }
