006198aa47387c9df40ead15807e27e2197bd5f2
AmihayHassan <57641622+AmihayHassan@users.noreply.github.com>
2021-04-21 16:39:41 +0300

    finished camera move and turn (might need future changes).
    finished documentation

--- src/elements/Camera.java
+++ src/elements/Camera.java
@@ -6,50 +6,63 @@ import static primitives.Util.isZero;
 
 public class Camera {
 
-    final private Point3D _P0;
-    final private Vector _vUP;
-    final private Vector _vTO;
-    final private Vector _vRIGHT;
+    /**
+     * @member _P0 - the camera location
+     * @member _Vup - Y axis vector
+     * @member _Vto - X axis vector
+     * @member _Vright - Z axis vector
+     *
+     * @member _width - object's actual width
+     * @member _height - object's actual height
+     * @member _distance - object's actual distance from the camera center
+     */
+
+    private Point3D _P0;
+    private Vector _Vup;
+    private Vector _Vto;
+    private Vector _Vright;
 
     private double _width;
     private double _height;
     private double _distance;
 
-    public Camera(Point3D p0, Vector vTO, Vector vUP) {
+    /**
+     * constructor
+     *
+     * @param p0 - the camera location
+     * @param Vto - Y axis vector
+     * @param Vup - X axis vector
+     */
+    public Camera(Point3D p0, Vector Vto, Vector Vup) {
 
-        if (!isZero(vUP.dotProduct(vTO))) {
+        if (!isZero(Vup.dotProduct(Vto))) {
             throw new IllegalArgumentException("up vector and  to vector arent orthogonal");
         }
 
         _P0 = p0;
-        _vUP = vUP.normalized();
-        _vTO = vTO.normalized();
-        _vRIGHT = _vTO.crossProduct(_vUP);
+        _Vup = Vup.normalized();
+        _Vto = Vto.normalized();
+
+        // _Vright is always the cross product of _Vto and _Vup since they are orthogonal by definition
+        _Vright = _Vto.crossProduct(_Vup);
     }
 
     public Point3D getP0() {
         return _P0;
     }
 
-    public Vector getvUP() {
-        return _vUP;
+    public Vector getVup() {
+        return _Vup;
     }
 
-    public Vector getvTO() {
-        return _vTO;
+    public Vector getVto() {
+        return _Vto;
     }
 
-    public Vector getvRIGHT() {
-        return _vRIGHT;
+    public Vector getVright() {
+        return _Vright;
     }
 
-//    public double getWidth() {
-//        return _width;
-//    }
-//
-//    public double getHeight() {
-//        return _height;
-//    }
 
     //chaining methods
     public Camera setViewPlaneSize(double width, double height) {
@@ -63,21 +76,24 @@ public class Camera {
         return this;
     }
 
-//    public double getDistance() {
-//        return _distance;
-//    }
 
     /**
+     * this function gets the view plane size and a selected pixel,
+     * and return the ray from the camera which intersects this pixel
+     *
      * @param nX - amount of rows in view plane (number of pixels)
      * @param nY - amount of columns in view plane (number of pixels)
-     * @param j - X's index
-     * @param i - Y's index
+     * @param j  - X's index
+     * @param i  - Y's index
+     *
      * @return - the ray which goes through the pixel
      */
+
     public Ray constructRayThroughPixel(int nX, int nY, int j, int i) {
 
-        Point3D Pc = _P0.add(_vTO.scale(_distance));
+        Point3D Pc = _P0.add(_Vto.scale(_distance)); // image center
 
+        // ratio (pixel width and height)
         double Rx = _width / nX;
         double Ry = _height / nY;
 
@@ -86,13 +102,61 @@ public class Camera {
         double Xj = Rx * (j - (nX - 1) / 2d);
 
         if (!isZero(Xj)) {
-            Pij = Pij.add(_vRIGHT.scale(Xj));
+            Pij = Pij.add(_Vright.scale(Xj));
         }
         if (!isZero(Yi)) {
-            Pij = Pij.add(_vUP.scale(Yi));
+            Pij = Pij.add(_Vup.scale(Yi));
         }
-        Vector Vij = Pij.subtract(_P0);
+
+        Vector Vij = Pij.subtract(_P0); // vector to pixel center
 
         return new Ray(_P0, Vij);
+    }
+
+    /**
+     * function to set new camera location
+     *
+     * @param up    add to _vUp vector
+     * @param to    add to _vTo vector
+     * @param right add to _vRight vector
+     * @return the camera in its new position
+     */
+
+    public Camera moveCamera(double up, double to, double right) {
+
+        if (!isZero(up)) {
+            this._P0.add(_Vup.scale(up));
+        }
+
+        if (!isZero(to)) {
+            this._P0.add(_Vto.scale(to));
+        }
+
+        if (!isZero(right)) {
+            this._P0.add(_Vright.scale(right));
+        }
+
+        return this;
+    }
+
+
+    // TODO: check if vector needs to be rotated by angle
+    // TODO: vector rotation : https://stackoverflow.com/questions/31225062/rotating-a-vector-by-angle-and-axis-in-java\
+
+    /**
+     * function to set new direction vectors to the camera
+     *
+     * @param Vup    - new Vup vector
+     * @param Vto    - n
+     * @param Vright - new Vright vector
+     * @return the camera after the rotation
+     */
+    public Camera turnCamera(Vector Vup, Vector Vto, Vector Vright) {
+
+        this._Vup = Vup;
+        this._Vto = Vto;
+        this._Vright = Vright;
+
+        return this;
     }
 }
