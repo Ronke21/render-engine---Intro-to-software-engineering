144c2c0b2c79247c3bdac6c96b64bfe8bedf397f
AmihayHassan <amihassan2@gmail.com>
2021-06-01 10:14:27 +0300

    MP1

--- tests/renderer/MP1.java
+++ tests/renderer/MP1.java
@@ -25,34 +25,39 @@ public class MP1 {
                 new Point3D(70, 60, 10),
                 new Vector(-5, -5, -1),
                 new Vector(-2.52, -2.52, 25.2),
-                1.5, 65, 81, true)
+                10, 85, 1, true)
                 .setViewPlaneSize(250, 250)
                 .setDistance(300);
 
-        scene.setBackground(Color.BLUE.add(Color.GREEN.reduce(2)).reduce(1.2));
-        scene.setAmbientLight(new AmbientLight(new Color(java.awt.Color.WHITE), 0.15));
+        int pixels = 250;
+
+        scene.setBackground(Color.BLUE.add(Color.GREEN.reduce(2)).reduce(5));
+        scene.setAmbientLight(new AmbientLight(Color.WHITE.reduce(5), 0.1));
 
         Color naturalGreen = new Color(Color.GREEN.add(Color.RED.reduce(10)).reduce(4));
         Color naturalGreen2 = naturalGreen.add(Color.GREEN.reduce(5));
 
-
         //region Tree1 points
         // create all relevant points
-        Point3D a = new Point3D(-14, 1, 1);
-        Point3D b = new Point3D(1, -14, 1);
-        Point3D c = new Point3D(14, 1, 1);
-        Point3D d = new Point3D(1, 14, 1);
-        Point3D f = new Point3D(-12, 1, 10);
-        Point3D g = new Point3D(1, -12, 10);
-        Point3D h = new Point3D(12, 1, 10);
-        Point3D i = new Point3D(1, 12, 10);
-        Point3D j = new Point3D(1, 1, 30);
-        Point3D k = new Point3D(-10, 1, 20);
-        Point3D l = new Point3D(1, -10, 20);
-        Point3D m = new Point3D(10, 1, 20);
-        Point3D n = new Point3D(1, 10, 20);
-        Point3D o = new Point3D(1, 1, 40);
-        Point3D p = new Point3D(1, 1, 20);
+        double xMoveTree = 0;
+        double yMoveTree = -5;
+        double zMoveTree = 0;
+
+        Point3D a = new Point3D(-14 + xMoveTree, 1 + yMoveTree, 1 + zMoveTree);
+        Point3D b = new Point3D(1 + xMoveTree, -14 + yMoveTree, 1 + zMoveTree);
+        Point3D c = new Point3D(14 + xMoveTree, 1 + yMoveTree, 1 + zMoveTree);
+        Point3D d = new Point3D(1 + xMoveTree, 14 + yMoveTree, 1 + zMoveTree);
+        Point3D f = new Point3D(-12 + xMoveTree, 1 + yMoveTree, 10 + zMoveTree);
+        Point3D g = new Point3D(1 + xMoveTree, -12 + yMoveTree, 10 + zMoveTree);
+        Point3D h = new Point3D(12 + xMoveTree, 1 + yMoveTree, 10 + zMoveTree);
+        Point3D i = new Point3D(1 + xMoveTree, 12 + yMoveTree, 10 + zMoveTree);
+        Point3D j = new Point3D(1 + xMoveTree, 1 + yMoveTree, 30 + zMoveTree);
+        Point3D k = new Point3D(-10 + xMoveTree, 1 + yMoveTree, 20 + zMoveTree);
+        Point3D l = new Point3D(1 + xMoveTree, -10 + yMoveTree, 20 + zMoveTree);
+        Point3D m = new Point3D(10 + xMoveTree, 1 + yMoveTree, 20 + zMoveTree);
+        Point3D n = new Point3D(1 + xMoveTree, 10 + yMoveTree, 20 + zMoveTree);
+        Point3D o = new Point3D(1 + xMoveTree, 1 + yMoveTree, 40 + zMoveTree);
+        Point3D p = new Point3D(1 + xMoveTree, 1 + yMoveTree, 20 + zMoveTree);
         //endregion
 
         //region Tree1 polygons
@@ -106,21 +111,25 @@ public class MP1 {
 
         //region Tree2 points
         // create all relevant points
-        a = new Point3D(-34, 6, 1);
-        b = new Point3D(-19, -9, 1);
-        c = new Point3D(-6, 6, 1);
-        d = new Point3D(-19, 19, 1);
-        f = new Point3D(-32, 6, 10);
-        g = new Point3D(-19, -7, 10);
-        h = new Point3D(-8, 6, 10);
-        i = new Point3D(-19, 17, 10);
-        j = new Point3D(-19, 6, 30);
-        k = new Point3D(-30, 6, 20);
-        l = new Point3D(-19, -5, 20);
-        m = new Point3D(-10, 6, 20);
-        n = new Point3D(-19, 15, 20);
-        o = new Point3D(-19, 6, 40);
-        p = new Point3D(-19, 6, 20);
+        xMoveTree = -40;
+        yMoveTree = 5;
+        zMoveTree = 0;
+
+        a = new Point3D(-14 + xMoveTree, 1 + yMoveTree, 1 + zMoveTree);
+        b = new Point3D(1 + xMoveTree, -14 + yMoveTree, 1 + zMoveTree);
+        c = new Point3D(14 + xMoveTree, 1 + yMoveTree, 1 + zMoveTree);
+        d = new Point3D(1 + xMoveTree, 14 + yMoveTree, 1 + zMoveTree);
+        f = new Point3D(-12 + xMoveTree, 1 + yMoveTree, 10 + zMoveTree);
+        g = new Point3D(1 + xMoveTree, -12 + yMoveTree, 10 + zMoveTree);
+        h = new Point3D(12 + xMoveTree, 1 + yMoveTree, 10 + zMoveTree);
+        i = new Point3D(1 + xMoveTree, 12 + yMoveTree, 10 + zMoveTree);
+        j = new Point3D(1 + xMoveTree, 1 + yMoveTree, 30 + zMoveTree);
+        k = new Point3D(-10 + xMoveTree, 1 + yMoveTree, 20 + zMoveTree);
+        l = new Point3D(1 + xMoveTree, -10 + yMoveTree, 20 + zMoveTree);
+        m = new Point3D(10 + xMoveTree, 1 + yMoveTree, 20 + zMoveTree);
+        n = new Point3D(1 + xMoveTree, 10 + yMoveTree, 20 + zMoveTree);
+        o = new Point3D(1 + xMoveTree, 1 + yMoveTree, 40 + zMoveTree);
+        p = new Point3D(1 + xMoveTree, 1 + yMoveTree, 20 + zMoveTree);
         //endregion
 
         //region Tree2 polygons
@@ -173,21 +182,25 @@ public class MP1 {
 
         //region Tree3 points
         // create all relevant points
-        a = new Point3D(6, -4, 1);
-        b = new Point3D(21, -19, 1);
-        c = new Point3D(34, -4, 1);
-        d = new Point3D(21, 9, 1);
-        f = new Point3D(8, -4, 10);
-        g = new Point3D(21, -17, 10);
-        h = new Point3D(32, -4, 10);
-        i = new Point3D(21, 7, 10);
-        j = new Point3D(21, -4, 30);
-        k = new Point3D(10, -4, 20);
-        l = new Point3D(21, -15, 20);
-        m = new Point3D(30, -4, 20);
-        n = new Point3D(21, 5, 20);
-        o = new Point3D(21, -4, 40);
-        p = new Point3D(21, -4, 20);
+        xMoveTree = -40;
+        yMoveTree = -20;
+        zMoveTree = 0;
+
+        a = new Point3D(-14 + xMoveTree, 1 + yMoveTree, 1 + zMoveTree);
+        b = new Point3D(1 + xMoveTree, -14 + yMoveTree, 1 + zMoveTree);
+        c = new Point3D(14 + xMoveTree, 1 + yMoveTree, 1 + zMoveTree);
+        d = new Point3D(1 + xMoveTree, 14 + yMoveTree, 1 + zMoveTree);
+        f = new Point3D(-12 + xMoveTree, 1 + yMoveTree, 10 + zMoveTree);
+        g = new Point3D(1 + xMoveTree, -12 + yMoveTree, 10 + zMoveTree);
+        h = new Point3D(12 + xMoveTree, 1 + yMoveTree, 10 + zMoveTree);
+        i = new Point3D(1 + xMoveTree, 12 + yMoveTree, 10 + zMoveTree);
+        j = new Point3D(1 + xMoveTree, 1 + yMoveTree, 30 + zMoveTree);
+        k = new Point3D(-10 + xMoveTree, 1 + yMoveTree, 20 + zMoveTree);
+        l = new Point3D(1 + xMoveTree, -10 + yMoveTree, 20 + zMoveTree);
+        m = new Point3D(10 + xMoveTree, 1 + yMoveTree, 20 + zMoveTree);
+        n = new Point3D(1 + xMoveTree, 10 + yMoveTree, 20 + zMoveTree);
+        o = new Point3D(1 + xMoveTree, 1 + yMoveTree, 40 + zMoveTree);
+        p = new Point3D(1 + xMoveTree, 1 + yMoveTree, 20 + zMoveTree);
         //endregion
 
         //region Tree3 polygons
@@ -238,36 +251,225 @@ public class MP1 {
         scene.geometries.addAll(greenTriangles);
         //endregion
 
-        //region create the trees' trunk and add them to the scene's geometries
-        // trunk for Tree1
+        //region Tree4 points
+        // create all relevant points
+        xMoveTree = 5;
+        yMoveTree = -20;
+        zMoveTree = 0;
+
+        a = new Point3D(-14 + xMoveTree, 1 + yMoveTree, 1 + zMoveTree);
+        b = new Point3D(1 + xMoveTree, -14 + yMoveTree, 1 + zMoveTree);
+        c = new Point3D(14 + xMoveTree, 1 + yMoveTree, 1 + zMoveTree);
+        d = new Point3D(1 + xMoveTree, 14 + yMoveTree, 1 + zMoveTree);
+        f = new Point3D(-12 + xMoveTree, 1 + yMoveTree, 10 + zMoveTree);
+        g = new Point3D(1 + xMoveTree, -12 + yMoveTree, 10 + zMoveTree);
+        h = new Point3D(12 + xMoveTree, 1 + yMoveTree, 10 + zMoveTree);
+        i = new Point3D(1 + xMoveTree, 12 + yMoveTree, 10 + zMoveTree);
+        j = new Point3D(1 + xMoveTree, 1 + yMoveTree, 30 + zMoveTree);
+        k = new Point3D(-10 + xMoveTree, 1 + yMoveTree, 20 + zMoveTree);
+        l = new Point3D(1 + xMoveTree, -10 + yMoveTree, 20 + zMoveTree);
+        m = new Point3D(10 + xMoveTree, 1 + yMoveTree, 20 + zMoveTree);
+        n = new Point3D(1 + xMoveTree, 10 + yMoveTree, 20 + zMoveTree);
+        o = new Point3D(1 + xMoveTree, 1 + yMoveTree, 40 + zMoveTree);
+        p = new Point3D(1 + xMoveTree, 1 + yMoveTree, 20 + zMoveTree);
+        //endregion
+
+        //region Tree4 polygons
+        BottomPyramidBottom = new Polygon(a, b, c, d);
+        // tree's triangles
+        BottomPyramid1 = new Triangle(a, b, p);
+        BottomPyramid2 = new Triangle(b, c, p);
+        BottomPyramid3 = new Triangle(c, d, p);
+        BottomPyramid4 = new Triangle(a, d, p);
+
+        MiddlePyramid1 = new Triangle(f, g, j);
+        MiddlePyramid2 = new Triangle(g, h, j);
+        MiddlePyramid3 = new Triangle(h, i, j);
+        MiddlePyramid4 = new Triangle(i, f, j);
+
+        TopPyramid1 = new Triangle(k, l, o);
+        TopPyramid2 = new Triangle(l, m, o);
+        TopPyramid3 = new Triangle(m, n, o);
+        TopPyramid4 = new Triangle(n, k, o);
+        //endregion
+
+        //region color Tree4 polygons and add to scene's geometries
+        greenTriangles = Arrays.asList(
+                BottomPyramidBottom,
+                BottomPyramid1,
+                BottomPyramid2,
+                BottomPyramid3,
+                BottomPyramid4,
+                MiddlePyramid1,
+                MiddlePyramid2,
+                MiddlePyramid3,
+                MiddlePyramid4,
+                TopPyramid1,
+                TopPyramid2,
+                TopPyramid3,
+                TopPyramid4
+        );
+
+        for (Geometry geo : greenTriangles) {
+            geo.setEmission(naturalGreen)
+                    .setMaterial(new Material()
+                            .setKd(0.01)
+                            .setKs(0.0001)
+                            .setShininess(2)
+                            .setKr(0.01));
+        }
+
+        scene.geometries.addAll(greenTriangles);
+        //endregion
+
+
+        //region Tree5 points
+        // create all relevant points
+        xMoveTree = -15;
+        yMoveTree = 10;;
+        zMoveTree = 0;
+
+        a = new Point3D(-14 + xMoveTree, 1 + yMoveTree, 1 + zMoveTree);
+        b = new Point3D(1 + xMoveTree, -14 + yMoveTree, 1 + zMoveTree);
+        c = new Point3D(14 + xMoveTree, 1 + yMoveTree, 1 + zMoveTree);
+        d = new Point3D(1 + xMoveTree, 14 + yMoveTree, 1 + zMoveTree);
+        f = new Point3D(-12 + xMoveTree, 1 + yMoveTree, 10 + zMoveTree);
+        g = new Point3D(1 + xMoveTree, -12 + yMoveTree, 10 + zMoveTree);
+        h = new Point3D(12 + xMoveTree, 1 + yMoveTree, 10 + zMoveTree);
+        i = new Point3D(1 + xMoveTree, 12 + yMoveTree, 10 + zMoveTree);
+        j = new Point3D(1 + xMoveTree, 1 + yMoveTree, 30 + zMoveTree);
+        k = new Point3D(-10 + xMoveTree, 1 + yMoveTree, 20 + zMoveTree);
+        l = new Point3D(1 + xMoveTree, -10 + yMoveTree, 20 + zMoveTree);
+        m = new Point3D(10 + xMoveTree, 1 + yMoveTree, 20 + zMoveTree);
+        n = new Point3D(1 + xMoveTree, 10 + yMoveTree, 20 + zMoveTree);
+        o = new Point3D(1 + xMoveTree, 1 + yMoveTree, 40 + zMoveTree);
+        p = new Point3D(1 + xMoveTree, 1 + yMoveTree, 20 + zMoveTree);
+        //endregion
+
+        //region Tree5 polygons
+        BottomPyramidBottom = new Polygon(a, b, c, d);
+        // tree's triangles
+        BottomPyramid1 = new Triangle(a, b, p);
+        BottomPyramid2 = new Triangle(b, c, p);
+        BottomPyramid3 = new Triangle(c, d, p);
+        BottomPyramid4 = new Triangle(a, d, p);
+
+        MiddlePyramid1 = new Triangle(f, g, j);
+        MiddlePyramid2 = new Triangle(g, h, j);
+        MiddlePyramid3 = new Triangle(h, i, j);
+        MiddlePyramid4 = new Triangle(i, f, j);
+
+        TopPyramid1 = new Triangle(k, l, o);
+        TopPyramid2 = new Triangle(l, m, o);
+        TopPyramid3 = new Triangle(m, n, o);
+        TopPyramid4 = new Triangle(n, k, o);
+        //endregion
+
+        //region color Tree5 polygons and add to scene's geometries
+        // add all of them to a list to apply settings (e.g. color) to all of them easily
+        greenTriangles = Arrays.asList(
+                BottomPyramidBottom,
+                BottomPyramid1,
+                BottomPyramid2,
+                BottomPyramid3,
+                BottomPyramid4,
+                MiddlePyramid1,
+                MiddlePyramid2,
+                MiddlePyramid3,
+                MiddlePyramid4,
+                TopPyramid1,
+                TopPyramid2,
+                TopPyramid3,
+                TopPyramid4
+        );
+
+        for (Geometry geo : greenTriangles) {
+            geo.setEmission(naturalGreen)
+                    .setMaterial(new Material()
+                            .setKd(0.01)
+                            .setKs(0.0001)
+                            .setShininess(2)
+                            .setKr(0.01));
+        }
+
+        scene.geometries.addAll(greenTriangles);
+        //endregion
+
+        //region create the trees' trunks and add them to the scene's geometries
+
+        //region trunk for Tree1
+        double xMoveTrunk = 0;
+        double yMoveTrunk = -5;
+        double zMoveTrunk = 0;
+
         Tube trunk = new Cylinder(
                 new Ray(
-                        new Point3D(0, 0, -10),
+                        new Point3D(0 + xMoveTrunk, 0 + yMoveTrunk, -10 + zMoveTrunk),
                         new Vector(0, 0, 1))
                 , 2.5,
                 15);
         trunk.setEmission(new Color(180, 83, 0).reduce(5));
         scene.geometries.add(trunk);
+        //endregion
 
-        // trunk for Tree2
+        //region trunk for Tree2
+        xMoveTrunk = -40;
+        yMoveTrunk = 5;
+        zMoveTrunk = 0;
         trunk = new Cylinder(
                 new Ray(
-                        new Point3D(-20, 5, -10),
+                        new Point3D(0 + xMoveTrunk, 0 + yMoveTrunk, -10 + zMoveTrunk),
                         new Vector(0, 0, 1))
                 , 2.5,
                 15);
         trunk.setEmission(new Color(180, 83, 0).reduce(5));
         scene.geometries.add(trunk);
+        //endregion
 
-        // trunk for Tree3
+        //region trunk for Tree3
+        xMoveTrunk = -40;
+        yMoveTrunk = -20;
+        zMoveTrunk = 0;
         trunk = new Cylinder(
                 new Ray(
-                        new Point3D(20, -5, -10),
+                        new Point3D(0 + xMoveTrunk, 0 + yMoveTrunk, -10 + zMoveTrunk),
                         new Vector(0, 0, 1))
                 , 2.5,
                 15);
         trunk.setEmission(new Color(180, 83, 0).reduce(5));
         scene.geometries.add(trunk);
+        //endregion
+
+        //region trunk for Tree4
+        xMoveTrunk = 5;
+        yMoveTrunk = -20;
+        zMoveTrunk = 0;
+        trunk = new Cylinder(
+                new Ray(
+                        new Point3D(0 + xMoveTrunk, 0 + yMoveTrunk, -10 + zMoveTrunk),
+                        new Vector(0, 0, 1))
+                , 2.5,
+                15);
+        trunk.setEmission(new Color(180, 83, 0).reduce(5));
+        scene.geometries.add(trunk);
+        //endregion
+
+        //region trunk for Tree5
+        xMoveTrunk = -15;
+        yMoveTrunk = 10;
+        zMoveTrunk = 0;
+
+        trunk = new Cylinder(
+                new Ray(
+                        new Point3D(0 + xMoveTrunk, 0 + yMoveTrunk, -10 + zMoveTrunk),
+                        new Vector(0, 0, 1))
+                , 2.5,
+                15);
+        trunk.setEmission(new Color(180, 83, 0).reduce(5));
+        scene.geometries.add(trunk);
+        //endregion
+
+
         //endregion
 
         //region add the ground plane
@@ -278,12 +480,14 @@ public class MP1 {
         );
 
         ground.setEmission(Color.BLUE.add(Color.GREEN.reduce(3)).reduce(10))
-                .setMaterial(new Material().setKd(0.03).setKs(0.00005).setKr(0.8));
+                .setMaterial(new Material().setKd(0.025).setKs(0.00005).setKr(0.8));
 
         scene.geometries.add(ground);
         //endregion
 
-        Sphere sun = new Sphere(new Point3D(10, -40, 30), 10);
+        //region moon
+        Point3D moonPoint = new Point3D(-10, -20, 30);
+        Sphere sun = new Sphere(moonPoint, 10);
         sun.setEmission(Color.YELLOW.add(Color.RED.reduce(6)))
                 .setMaterial(
                         new Material()
@@ -292,9 +496,27 @@ public class MP1 {
                                 .setShininess(50)
                                 .setKt(0.9)
                 );
-
-
         scene.geometries.add(sun);
+
+        scene.lights.add(
+                new PointLight(
+                        Color.WHITE.reduce(2).add(Color.YELLOW).scale(3),
+                        moonPoint)
+        );
+        //endregion
+
+        Point3D starPoint = new Point3D(-40, -100, 30);
+        sun = new Sphere(moonPoint, 10);
+        sun.setEmission(Color.YELLOW)
+                .setMaterial(
+                        new Material()
+                                .setKd(0.005)
+                                .setKs(0.00005)
+                                .setShininess(50)
+                                .setKt(0.9)
+                );
+        scene.geometries.add(sun);
+
 
         scene.lights.add(
                 new SpotLight(
@@ -305,11 +527,11 @@ public class MP1 {
                         .setkQ(0.0000006));
 
         scene.lights.add(
-                new PointLight(Color.WHITE.reduce(2).add(Color.YELLOW).scale(3), new Point3D(10, -40, 30)));
+                new PointLight(
+                        Color.WHITE.reduce(2).add(Color.YELLOW).scale(3),
+                        moonPoint)
+        );
 
-        int pixels = 1000;
-
-        //70, 60, 10
         Render render = new Render() //
                 .setImageWriter(new ImageWriter("TreeTestDOF_MP1", pixels, pixels)) //
                 .setCamera(camera) //
